e9ec4a0c3bbc827701fc8535ad78ee7fc9aa0e82
Alex Curtis <jacurtis@hey.com>
2023-07-08 22:14:14 -0600

    feat: Update sidebar nav and base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://jacurtis.com',
   integrations: [mdx(), sitemap(), tailwind(), image(
     {
       serviceEntryPoint: '@astrojs/image/sharp',
