f568ae3eecdccf3ceba35e1801c67bb786633080
Alex Curtis <jacurtis@hey.com>
2023-12-16 21:12:27 -0700

    init: From Astrofy

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Alex Curtis ⚡';
+export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
