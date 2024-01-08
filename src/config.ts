3056b468465f734fa1d01f30ce2a50354c8571ea
Alex Curtis <jacurtis@hey.com>
2024-01-07 22:02:07 -0700

    refactor: Improve title and header inheritance

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Alex Curtis ⚡';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'J. Alexander Curtis | Infrastructure Engineer & Cybersecurity Expert';
+export const SITE_DESCRIPTION = '';
+
+export const SITE_HEADER = 'Alex Curtis ⚡';
