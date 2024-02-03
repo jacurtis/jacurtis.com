ae0b47915f8005011f99a7bd7c6067c2737e7999
Alex Curtis <jacurtis@hey.com>
2024-02-03 15:27:30 -0700

    feat: Add Icons component and update SideBarFooter.astro Icons. (Resolves #9)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: 'https://jacurtis.com',
+  integrations: [mdx(), sitemap(), tailwind(), icon()]
 });
