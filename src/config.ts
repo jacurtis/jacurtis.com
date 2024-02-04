38f8243be4dd25e4ff2490b8953b4c580f003321
Alex Curtis <jacurtis@hey.com>
2024-02-04 15:54:55 -0700

    feat: Update Title, and fix menu navigation

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'J. Alexander Curtis | Infrastructure Engineer & Cybersecurity Expert';
+export const SITE_TITLE = 'J. Alexander Curtis | Cloud Engineer & Cybersecurity Expert';
 export const SITE_DESCRIPTION = '';
 
 export const SITE_HEADER = 'Alex Curtis ⚡';
